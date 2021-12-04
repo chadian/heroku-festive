30a32bfd9a27b5c6af6fa47a223a88acf452aefe
Chad Carbert <ccarbert@salesforce.com>
2021-12-04 14:38:14 +0000

    Improve Scrollbar positioning

--- src/components/Scrollbar.tsx
+++ src/components/Scrollbar.tsx
@@ -5,12 +5,11 @@ export function ScrollBar({ height: barHeight, start, end, total }: any) {
   const BAR = '▒';
   const BAR_BG = ' ';
 
-  const startOnBar = Math.round(barHeight * (start / total));
-  const endOnBar = Math.ceil(barHeight * (end / total));
+  const startOnBar = Math.ceil(barHeight * (start / total));
+  const endOnBar = Math.floor(barHeight * (end / total));
 
   const barSegments = Array(barHeight).fill('').map((_, i) => {
-    i = i + 1;
-    return i >= startOnBar && i <= endOnBar ? BAR : BAR_BG;
+    return i > startOnBar && i < endOnBar ? BAR : BAR_BG;
   });
 
   return <Box><Text>{`-\n${barSegments.join('\n')}\n-`}</Text></Box>;
